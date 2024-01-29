9eb5222aace9bfdfc3bbf07ea6c3a54a739629c2
Kondzioplus <111319052+Kondzioplus@users.noreply.github.com>
2024-01-29 12:30:25 +0100

    Tworzenie formularza w React

---
+++ src/User/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const user = (props) => {
+    return <div>
+        <h2>Username: {props.username}</h2>
+    <h3>Role: {props.role}</h3>
+    <p>State: {props.children}</p>
+    </div>
+}
+
+export default user;
